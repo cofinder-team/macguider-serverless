b49190a708882157cf81d7826abb4fd6002c1b34
Seungjik Lee <lsjbh45@gmail.com>
2023-07-09 17:37:15 +0900

    feat: Add database connection to handler

---
+++ src/datasource.ts
@@ -0,0 +1,15 @@
+import { DataSource } from 'typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  synchronize: false,
+  logging: false,
+  namingStrategy: new SnakeNamingStrategy(),
+  entities: [],
+});
--- src/handler.ts
+++ src/handler.ts
@@ -1,8 +1,18 @@
-import { Handler, Context } from 'aws-lambda';
+import { Handler, Context, APIGatewayProxyEvent } from 'aws-lambda';
+import { AppDataSource } from './datasource';
+import { DataSource } from 'typeorm';
 
-const run: Handler = async (event: any, context: Context) => {
+const run: Handler = async (event: APIGatewayProxyEvent, context: Context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
   console.log('Event: ', event);
-  console.log('env', process.env);
+
+  try {
+    const dataSource: DataSource = await AppDataSource.initialize();
+    console.log('datasource initialized');
+    dataSource.destroy();
+  } catch (error) {
+    throw error;
+  }
 };
 
 export { run };
