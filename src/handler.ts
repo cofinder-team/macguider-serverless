e4523f10d9e2b3f826a189f0ffaa2ae8922b2df8
Seungjik Lee <lsjbh45@gmail.com>
2023-07-09 11:13:23 +0900

    chore: Add lint settings

--- src/handler.ts
+++ src/handler.ts
@@ -1,7 +1,7 @@
-import { Handler, Context } from "aws-lambda";
+import { Handler, Context } from 'aws-lambda';
 
 const run: Handler = async (event: any, context: Context) => {
-  console.log("Event: ", event);
+  console.log('Event: ', event);
 };
 
 export { run };
