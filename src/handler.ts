947396d958f9f8936f417348b0218aa83fad8904
Seungjik Lee <lsjbh45@gmail.com>
2023-07-09 13:53:06 +0900

    chore: Add settings for environment management

--- src/handler.ts
+++ src/handler.ts
@@ -2,6 +2,7 @@ import { Handler, Context } from 'aws-lambda';
 
 const run: Handler = async (event: any, context: Context) => {
   console.log('Event: ', event);
+  console.log('env', process.env);
 };
 
 export { run };
