48191486bc8f2d3523ae33ce4d94e8f5107d7a25
Seungjik Lee <lsjbh45@gmail.com>
2023-08-07 18:10:26 +0900

    feat: Enhance mail service by configuring template

--- src/services/mail.service.ts
+++ src/services/mail.service.ts
@@ -1,5 +1,8 @@
 import { Transporter, createTransport } from 'nodemailer';
 import SMTPTransport from 'nodemailer/lib/smtp-transport';
+import { join } from 'path';
+import fs from 'fs';
+import Handlebars from 'handlebars';
 import Mail from 'nodemailer/lib/mailer';
 
 export class MailService {
@@ -22,7 +25,23 @@ export class MailService {
     this.transporter = createTransport(options, defaults);
   }
 
-  async sendMail(options: Mail.Options) {
-    return this.transporter.sendMail(options);
+  private createTemplate = (name: string) => {
+    const path = join(__dirname, `../assets/templates/pages/${name}.hbs`);
+    const source = fs.readFileSync(path, 'utf8');
+
+    return Handlebars.compile(source);
+  };
+
+  async sendMail(
+    options: Mail.Options & {
+      template: string;
+      context: unknown;
+    },
+  ) {
+    const { template: path, context: data, ...rest } = options;
+    const template: HandlebarsTemplateDelegate = this.createTemplate(path);
+    const html: string = template(data);
+
+    return this.transporter.sendMail({ ...rest, html });
   }
 }
